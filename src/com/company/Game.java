0311de9a8a9f6c53fd84f5e7ee2dfd3ed4cda35a
Kuba Szostko <kubaszostko@gmail.com>
2020-12-24 10:00:17 +0100

    connectend Command to JButton actionListener

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import com.company.commands.*;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -17,6 +21,7 @@ public class Game {
         frame = new JFrame("Arkanoid");
         frame.setSize(600, 500);
         frame.getContentPane().setBackground(Color.white);
+        frame.setLayout(new FlowLayout());
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -28,6 +33,21 @@ public class Game {
     }
 
     private void displayStartView() {
+        JButton rankingButton = new JButton("Show Ranking");
+        rankingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new ShowRankingCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        frame.add(rankingButton);
+
         frame.setVisible(true);
+    }
+
+    private void executeCommand(Command command) {
+        command.execute();
     }
 }
