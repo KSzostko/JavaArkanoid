04f5cfcf4b9d8c7bcd1fa69cf91dc8fb09c10ac9
Kuba Szostko <kubaszostko@gmail.com>
2020-12-25 10:25:07 +0100

    created method addNavButtons and finished start view

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -33,16 +33,20 @@ public class Game {
     }
 
     private void displayStartView() {
-        JButton rankingButton = new JButton("Show Ranking");
-        rankingButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                Command command = new ShowRankingCommand(Game.this);
-                executeCommand(command);
-            }
-        });
+        clearScreen();
+        frame.setLayout(new BorderLayout(20, 60));
 
-        frame.add(rankingButton);
+        JLabel titleLabel = new JLabel("Arkanoid", JLabel.CENTER);
+        titleLabel.setFont(new Font("serif", Font.BOLD, 25));
+
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+        controlPanel.setBackground(Color.WHITE);
+
+        frame.add(titleLabel, BorderLayout.PAGE_START);
+        frame.add(controlPanel, BorderLayout.CENTER);
+
+        addNavButtons(controlPanel, true);
 
         frame.setVisible(true);
     }
@@ -54,5 +58,78 @@ public class Game {
     private void clearScreen() {
         frame.getContentPane().removeAll();
         frame.validate();
+    }
+
+    private void addNavButtons(JPanel panel, boolean isStart) {
+        JButton startButton = new JButton("New Game");
+        startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton rankingButton = new JButton("Show Ranking");
+        rankingButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton authorsButton = new JButton("Authors");
+        authorsButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton goBackButton = new JButton("goBack");
+        exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new StartLevelCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        authorsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new ShowAuthorsCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new ExitCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        rankingButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new ShowRankingCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        goBackButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Command command = new GoBackCommand(Game.this);
+                executeCommand(command);
+            }
+        });
+
+        panel.add(startButton);
+        panel.add(Box.createVerticalStrut(20));
+
+        panel.add(authorsButton);
+        panel.add(Box.createVerticalStrut(20));
+
+        if(isStart) {
+            panel.add(rankingButton);
+        } else {
+            panel.add(goBackButton);
+        }
+        panel.add(Box.createVerticalStrut(20));
+
+        panel.add(exitButton);
+        panel.add(Box.createVerticalStrut(20));
     }
 }
