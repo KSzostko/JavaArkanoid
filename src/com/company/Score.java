35a104e4c8f43bc9b15f649d4f242597a8e84fc1
Kuba Szostko <kubaszostko@gmail.com>
2020-12-25 10:59:11 +0100

    created Score class

---
+++ src/com/company/Score.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Score {
+    private String username;
+    private int points;
+
+    public Score(String username, int points) {
+        this.username = username;
+        this.points = points;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+}
